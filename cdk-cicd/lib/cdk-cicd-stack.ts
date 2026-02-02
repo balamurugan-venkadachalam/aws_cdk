9db1023ab1488f50707d520c692eca7d4b7f26ce
Balamurugan Venkadachalam <engan.balamurugan@gmail.com>
2026-02-03 10:31:07 +1300

    - Change pipeline name from - Configure CodeBuild to use STANDARD_7_0 Linux build image - Update hello service response body to 'Hello! version 2'

--- cdk-cicd/lib/cdk-cicd-stack.ts
+++ cdk-cicd/lib/cdk-cicd-stack.ts
@@ -2,15 +2,25 @@ import * as cdk from 'aws-cdk-lib';
 import { CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import { PipelineStage } from './PipelineStage';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'AwesomePipeline', {
-      pipelineName: 'AwesomePipeline',
+    const pipeline = new CodePipeline(this, 'MyFirstPipeline', {
+      pipelineName: 'MyFirstPipeline',
+        codeBuildDefaults: {
+          buildEnvironment: {
+            buildImage: codebuild.LinuxBuildImage.STANDARD_7_0
+          }
+        },
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('alexhddev/CDK-course-resources', 'cicd-practice'),
+        input: CodePipelineSource.gitHub('balamurugan-venkadachalam/aws_cdk', 'cicd-practice',
+          {
+            authentication: cdk.SecretValue.secretsManager('github-token')
+          }
+        ),
         commands: [
           'cd cdk-cicd',
           'npm ci',
@@ -32,5 +42,5 @@ export class CdkCicdStack extends cdk.Stack {
       ]
     }))
 
-  }
+   }
 }
--- cdk-cicd/services/hello.ts
+++ cdk-cicd/services/hello.ts
@@ -5,7 +5,7 @@
 async function handler(event:any, context: any) {
     return {
         statusCode: 200,
-        body: 'Hello!'
+        body: 'Hello! version 2'
     }
 }
 
